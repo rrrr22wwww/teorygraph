d8fd049a64792113c4b4f2db0a9be32b3fd889b9
TrySquadDF <sjpdkedkdjkdkdjdmddd@gmail.com>
2025-09-26 02:58:49 +0300

    init

--- intex.ts
+++ intex.ts
@@ -1,4 +1,4 @@
-
+import * as fs from "fs"
 
 type typeGraphArg = {
     pathGraph:string,
@@ -6,9 +6,8 @@ type typeGraphArg = {
 }
 
 class graph {
-    private path:string;
-    private vertex:[];
-
+    private vertexWeight:[];
+    private filedata ;
     public size(){
         
     }
@@ -16,7 +15,7 @@ class graph {
         return 0
     }
 
-    public is_edge(v1: Object, v2: Object):boolean {
+    public is_edge(v1: Object, v2: Object): boolean {
         return true
     }
     public add_vertex(v1:Object):void {
@@ -25,8 +24,26 @@ class graph {
     public add_edge(v1: Object, v2: Object):void {
         
     }
-    constructor(pathGraph:string,typeGraph:string) {
-        this.path = pathGraph;
-        this.vertex = []
+    async readfile (tp:string) {
+        const file = Bun.file(tp);
+        const content = (await file.text()).split('\n');
+        const weightgraph = new Map();
+        for (let i = 1; i < content.length - 1;i++) {
+            const lineParts = content[i].split(" ");
+            const parsedLine = [];
+            for (let j = 0; j < lineParts.length; j++) {
+                parsedLine.push(lineParts[j].split(":"));
+            }
+            weightgraph.set(i,parsedLine)
+        }
+        console.log(weightgraph) // Сдлеать реализацию МАП ДЛЯ МАССИВА компанентов смежности элементов!!!
+    }
+     constructor (pathGraph:string,typeGraph:string) {
+        this.filedata = this.readfile(pathGraph)
+        // this.filedata = fs.readFileSync(pathGraph,"utf-8").split('\n')
+        console.log(this.filedata)
+        this.vertexWeight = []
     }
 }
+
+const Graph2 = new graph("/Users/root1/Desktop/tgraph/teorygraph/chek.txt","directed")
